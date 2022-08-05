60160e0f327bb8e80b172c93605dd2e098adf2db
Bernard  Mburu Huncho <87483225+fabulousDesigns@users.noreply.github.com>
2022-08-05 11:41:17 +0300

    Add files via upload

---
+++ js-refresher/main.js
@@ -0,0 +1,13 @@
+// Equality comparison
+
+const firstValue = 10
+const myArr = [100]
+
+console.log(firstValue == 20)
+console.log([100] == 100)
+// strict Equality comparison
+const secondValue = 10
+const mySecondArr = [100]
+
+console.log(secondValue == 20)
+console.log([100] == 100)
