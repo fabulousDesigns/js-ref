1dd028cecb156116db1b6151728b5861a1081724
fabulousdesigns <hazardmburu10@gmail.com>
2022-08-08 17:13:15 +0300

    more changes

--- js-refresher/main.js
+++ js-refresher/main.js
@@ -41,7 +41,35 @@ console.log(isError)
 // ==========================
 // 5. SPREAD OPERATOR
 // ==========================
+// Array
+const numbersOne = [1,2,3]
+console.log(numbersOne)
+const numbersTwo = [4,5,6]
+console.log(numbersTwo)
+const combinedArr = [...numbersOne, ...numbersTwo] 
+console.log(combinedArr)
+// destructure arr
+const [x,y, ...rest] = combinedArr
+console.log(x)
+console.log(y)
+console.log(...rest)
+// OBJECTS
 // add salary property to employee object
+const myVehicle = {
+    brand: 'Ford',
+    model: 'Mustang',
+    color: 'red'
+  }
+  console.log(myVehicle)
+  const updateMyVehicle = {
+    type: 'car',
+    year: 2021, 
+    color: 'yellow'
+  }
+  console.log(updateMyVehicle)
+const updatedVehicle  = {...myVehicle, ...updateMyVehicle}
+console.log(updatedVehicle)
+
 const includeSalary = true
 const Employee = {
     firstName: 'Bernard',
@@ -53,4 +81,26 @@ if(includeSalary){
     Employee.salary = 60000
 }
 console.log(Employee)
+const salary = 40000
+const newEmployee = {salary, ...Employee}
+console.log(newEmployee)
 
+
+const isLoggedIn = true
+
+const User = {
+  username : 'Huncho',
+  password : 1234567,
+  ...(isLoggedIn && {email: 'hazardmburu10@gmail.com'})
+}
+
+console.log(User)
+// ==========================
+// 6. REST OPERATOR
+// ==========================
+
+const findSum = (...args)=>{
+ console.log(args)
+}
+
+findSum(5,6,7)
---
+++ js-refresher/tryOut.js
@@ -0,0 +1,11 @@
+const user = {
+    firstName:'morioh',
+    lastName:'Huncho',
+    age:23
+
+}
+ console.table(user)
+ console.log(user)
+
+const userName = user.firstName.toUpperCase() + user.lastName.toUpperCase()
+console.log('Welcome \t' + userName)
